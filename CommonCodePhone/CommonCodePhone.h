210cdbc944f7686198f5b7761a8d82f9be011401
Jeremy Roberts <jeremy@jdrobert.com>
2017-10-15 09:53:01 -0500

    Shared code frameworks and watch app

---
+++ CommonCodePhone/CommonCodePhone.h
@@ -0,0 +1,19 @@
+//
+//  CommonCodePhone.h
+//  CommonCodePhone
+//
+//  Created by Jeremy Roberts on 10/15/17.
+//  Copyright © 2017 Quiver Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CommonCodePhone.
+FOUNDATION_EXPORT double CommonCodePhoneVersionNumber;
+
+//! Project version string for CommonCodePhone.
+FOUNDATION_EXPORT const unsigned char CommonCodePhoneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommonCodePhone/PublicHeader.h>
+
+
---
+++ CommonCodeWatch/CommonCodeWatch.h
@@ -0,0 +1,19 @@
+//
+//  CommonCodeWatch.h
+//  CommonCodeWatch
+//
+//  Created by Jeremy Roberts on 10/15/17.
+//  Copyright © 2017 Quiver Apps. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for CommonCodeWatch.
+FOUNDATION_EXPORT double CommonCodeWatchVersionNumber;
+
+//! Project version string for CommonCodeWatch.
+FOUNDATION_EXPORT const unsigned char CommonCodeWatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommonCodeWatch/PublicHeader.h>
+
+
